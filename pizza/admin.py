0401b2518010d230a465078a94a436c918306caf
srgokulnath <srgwilet@gmail.com>
2022-02-09 17:54:44 +0530

    features of forms, request.post, adding image in forms

--- pizza/admin.py
+++ pizza/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Pizza,Size
 
+admin.site.register(Pizza)
+admin.site.register(Size)
 # Register your models here.
--- pizza/views.py
+++ pizza/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
+from .forms import PizzaForm
 
 def home(request):
     return render(request, 'pizza/home.html')
 def order(request):
-    return render(request, 'pizza/order.html')
+    if request.method == 'post':
+        filled_form = PizzaForm(request.post, request.FILES)
+        if filled_form.is_valid:
+            topping1 = filled_form.cleaned_data['topping1']
+            topping2 = filled_form.cleaned_data['topping2']
+            size = filled_form.cleaned_data['size']
+            note = 'this is ur order %s %s and %s' %(topping1, topping2, size)
+            
+            new_form = PizzaForm()
+            return render(request, 'pizza/order.html', {'pizzaform': new_form, 
+            'note': note
+            }, context_instance=RequestContext(request))
+
+    else:    
+        form = PizzaForm()
+        return render(request, 'pizza/order.html', {'pizzaform':form})
 
 # Create your views here.
